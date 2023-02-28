04aa4265b6c5c36a9a9b8ccf06029c1e676d8e49
Koji Hisano <hisano@gmail.com>
2023-02-28 20:52:14 +0900

    detektプラグインを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = "jp.hisano"
